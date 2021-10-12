7a6cf4c00de8de85d92fe515ed96f5f36629858e
Glitch (aframe) <none>
2021-10-12 15:13:41 +0000

    🏆🛳 Checkpoint ./index.html:35297843/1946 ./cursorEvents.js:35297843/716

---
+++ cursorEvents.js
@@ -0,0 +1,24 @@
+AFRAME.registerComponent("cursor-listener", {
+  schema: {
+    selectedItemId: { default: "", type: "string" },
+  },
+  init: function () {
+    this.handleMouseEnterEvents();
+    this.handleMuseLeaveEvents();
+  },
+  handlePlacesListState: function () {
+    const id = this.el.getAttribute("id");
+    const placesId = ["taj-mahal", "budapest", "new-york-city", "eiffel-tower"];
+    if (placesId.includes(id)) {
+      const placeContainer = document.querySelector("#places-container");
+      placeContainer.setAttribute("cursor-listener", {
+        selectdItenId: id,
+      });
+      this.el.setAttribute("material", {
+        color:"#D02A30",
+        opacity: 0.6,
+      });
+    }
+  },
+  ha
+})
